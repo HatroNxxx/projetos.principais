0ca54a0e36cf4b962299ebb1c0886d0a4ec9a7d9
HatroNxxx <neqxsvitorcosta@gmail.com>
2024-02-13 19:02:36 -0300

    Initial commit

---
+++ node_3ee40897cf163.tmp.js
@@ -0,0 +1,12 @@
+let listaDeConvidado = ['loira', 'Alison', 'Caua', 'Vitor']
+let naoConvidados = ''
+
+for (let numeirosDeConvidado = 0; listaDeConvidado.length; numeirosDeConvidado ++) 
+
+
+
+if (naoConvidados) {
+    console.log('Não pode entra')
+} else {
+    console.log('Pode Entra')
+}
